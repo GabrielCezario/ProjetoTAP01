addec060ec6461417b6ca2ec2ab20acac76c5930
Felipe Marques Silva <felipe.m199@gmail.com>
2020-04-21 22:32:00 -0300

    Controller aperfeiçoado

--- src/Controller/AtividadeController.java
+++ src/Controller/AtividadeController.java
@@ -5,26 +5,31 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+import Model.Developer;
 import Model.SoftwareActivity;
-import Util.Enum.ActivityTime;
+import Util.Enum.DevelopmentExperience;
 
 public class AtividadeController {
 	
+	public AtividadeController() {
+		generateSoftwareActivity();
+	}
+ 	
 	private List<SoftwareActivity> listOfSoftwareActivity = new ArrayList<SoftwareActivity>();
 	private List<String> listNameOfSoftwareActivity = new ArrayList<String>();
-	private List<ActivityTime> listLvlOfSoftwareActivity = new ArrayList<ActivityTime>();
+	private List<DevelopmentExperience> listLvlOfSoftwareActivity = new ArrayList<DevelopmentExperience>();
 	
-	public List<SoftwareActivity> returnSoftwareActivity() {	
+	private List<SoftwareActivity> generateSoftwareActivity() {	
 		
 		int numberSoftwareActivity = generateNumberRange(150, 300);
 		
 		generateSoftwareActivityName();
 		generateSoftwareActivityLvl();
 		
+		
 		for (int i = 0; i < numberSoftwareActivity; i++) {
 			Collections.shuffle(listNameOfSoftwareActivity);
 			Collections.shuffle(listLvlOfSoftwareActivity);
-			
 			this.listOfSoftwareActivity.add(new SoftwareActivity(listNameOfSoftwareActivity.get(0), listLvlOfSoftwareActivity.get(0)));
 		}
 		
@@ -38,19 +43,23 @@ public class AtividadeController {
 		this.listNameOfSoftwareActivity.add("Analisando requisitos");
 		this.listNameOfSoftwareActivity.add("Desenvolvendo");
 		this.listNameOfSoftwareActivity.add("Criando testes");
+		this.listNameOfSoftwareActivity.add("Commitando codigo");
 		this.listNameOfSoftwareActivity.add("Corrigindo bugs");
 		this.listNameOfSoftwareActivity.add("Analisando backlog");
 	}
 	
 	private void generateSoftwareActivityLvl() {
-		this.listLvlOfSoftwareActivity.add(ActivityTime.JUNIOR);
-		this.listLvlOfSoftwareActivity.add(ActivityTime.MIDDLE);
-		this.listLvlOfSoftwareActivity.add(ActivityTime.SENIOR);
+		this.listLvlOfSoftwareActivity.add(DevelopmentExperience.JUNIOR);
+		this.listLvlOfSoftwareActivity.add(DevelopmentExperience.MIDDLE);
+		this.listLvlOfSoftwareActivity.add(DevelopmentExperience.SENIOR);
 	}
 	
-	public static int generateNumberRange(int min, int max) {
+	private static int generateNumberRange(int min, int max) {
         Random random = new Random();
         return random.nextInt((max - min) + 1) + min;
     }
+	public List<SoftwareActivity> getSoftActivityList() {
+		return this.listOfSoftwareActivity;
+	}
 
 }
